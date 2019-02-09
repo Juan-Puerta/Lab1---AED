e54b0fdd56bec62be055fc56541392be1e4f1e5f
Juan-Puerta <42070035+Juan-Puerta@users.noreply.github.com>
2019-02-09 16:43:54 -0500

    I added the Bairstow's method

--- Programa/src/model/Polynomial.java
+++ Programa/src/model/Polynomial.java
@@ -2,4 +2,48 @@ package model;
 
 public class Polynomial {
 
+	private int grade;
+	private double[] constant;
+	
+	private BairstowMethod theBairstow;
+	private NewtonMethod theNewton;
+	
+	public Polynomial(double[] constant) {
+		
+		this.constant = constant;
+		this.grade = constant.length;
+		
+	}
+
+	public int getGrade() {
+		return grade;
+	}
+
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+
+	public double[] getConstant() {
+		return constant;
+	}
+
+	public void setConstant(double[] constant) {
+		this.constant = constant;
+	}
+
+	public BairstowMethod getTheBairstow() {
+		return theBairstow;
+	}
+
+	public void setTheBairstow(BairstowMethod theBairstow) {
+		this.theBairstow = theBairstow;
+	}
+
+	public NewtonMethod getTheNewton() {
+		return theNewton;
+	}
+
+	public void setTheNewton(NewtonMethod theNewton) {
+		this.theNewton = theNewton;
+	}
 }
