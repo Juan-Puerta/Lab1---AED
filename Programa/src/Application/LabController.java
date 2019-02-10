d5d90e6a9b5df429b3627c62f1bb1c0110ea0e5d
camiilocq <38769518+camiilocq@users.noreply.github.com>
2019-02-10 14:40:11 -0500

    this has been changed

---
+++ Programa/src/Application/LabController.java
@@ -0,0 +1,27 @@
+package Application;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
+
+public class LabController {
+
+	private TextField TFInput;
+	private Button BSolve;
+	private TextField TFGenerated;
+	private Button BGenerate;
+	private TextField TFValueX;
+	private TextArea TASolution;
+	
+	public void initialize() {
+		
+	}
+	
+	public void solve() {
+		
+	}
+	
+	public void generate() {
+		
+	}
+}
