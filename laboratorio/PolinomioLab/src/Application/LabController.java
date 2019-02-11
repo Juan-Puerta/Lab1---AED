c99d78f71a5860db7137abdbc7986f5c54b7d183
camiilocq <38769518+camiilocq@users.noreply.github.com>
2019-02-11 14:45:09 -0500

    add the comments in this class

--- laboratorio/PolinomioLab/src/Application/LabController.java
+++ laboratorio/PolinomioLab/src/Application/LabController.java
@@ -9,7 +9,7 @@ public class LabController {
 	
 	@FXML private TextField TFInput;
 	@FXML private TextField TFGenerated;
-	@FXML private TextField TFSolution;
+	@FXML private TextField TFValueX;
 	@FXML private Button Bsolve;
 	@FXML private Button BGenerate;
 	@FXML private TextArea TASolution;
@@ -17,23 +17,29 @@ public class LabController {
 	public void initialize() {
 		TFGenerated.setEditable(false);
 		TASolution.setEditable(false);
-		TFSolution.setEditable(false);
+		TFValueX.setEditable(false);
 	}
 	
+	/**
+	 * this method solve the problem, it call method in the model
+	 */
 	public void solve() {
 		
 	}
 	
+	/**
+	 * this method generate a polynomial with grade n and n values
+	 */
 	public void generate() {
 		
 	}
 	
+	/**
+	 * this method show the process in a text area
+	 */
 	public void show(String value) {
-		TASolution.setText(value+'\n');
-		
+				
 	}
-	
-	
-	
+		
 
 }
