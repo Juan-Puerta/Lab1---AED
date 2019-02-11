bc8a70883ccabdebd4b6e0527704a611fedf5f68
camiilocq <38769518+camiilocq@users.noreply.github.com>
2019-02-11 14:34:31 -0500

    this has been changed

--- laboratorio/PolinomioLab/src/Application/LabController.java
+++ laboratorio/PolinomioLab/src/Application/LabController.java
@@ -1,32 +1,39 @@
 package Application;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class LabController {
-
-	private TextField TFInput;
-	private Button BSolve;
-	private TextField TFGenerated;
-	private Button BGenerate;
-	private TextField TFValueX;
-	private TextArea TASolution;
+	
+	@FXML private TextField TFInput;
+	@FXML private TextField TFGenerated;
+	@FXML private TextField TFSolution;
+	@FXML private Button Bsolve;
+	@FXML private Button BGenerate;
+	@FXML private TextArea TASolution;
 	
 	public void initialize() {
-		
+		TFGenerated.setEditable(false);
+		TASolution.setEditable(false);
+		TFSolution.setEditable(false);
 	}
 	
 	public void solve() {
-		show("solve");
+		
 	}
 	
 	public void generate() {
-		show("generate");
+		
 	}
 	
 	public void show(String value) {
 		TASolution.setText(value+'\n');
-		System.out.println(value);
+		
 	}
+	
+	
+	
+
 }
--- laboratorio/PolinomioLab/src/Application/Main.java
+++ laboratorio/PolinomioLab/src/Application/Main.java
@@ -5,11 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import model.Polynomial;
 
 public class Main extends Application {
 
+	private Polynomial polynomial;
 	public Main() {
-		
+//		setPolynomial(new Polynomial(constant));
 	}
 	
 	@Override
@@ -25,6 +27,14 @@ public class Main extends Application {
 		}
 	}
 	
+	public Polynomial getPolynomial() {
+		return polynomial;
+	}
+
+	public void setPolynomial(Polynomial polynomial) {
+		this.polynomial = polynomial;
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
