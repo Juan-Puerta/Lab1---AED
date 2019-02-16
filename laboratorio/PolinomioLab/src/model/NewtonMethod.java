2468477041dcbc948f025251c8fdc22f3fbdcbd7
Juan-Puerta <42070035+Juan-Puerta@users.noreply.github.com>
2019-02-16 15:00:22 -0500

    I added Newton's method

--- laboratorio/PolinomioLab/src/model/NewtonMethod.java
+++ laboratorio/PolinomioLab/src/model/NewtonMethod.java
@@ -2,4 +2,25 @@ package model;
 
 public class NewtonMethod {
 
+	double p,tol;
+	double a[];
+	
+	NewtonMethod(double[] a1, double x0_1, double tol_1){
+		p = x0_1;
+		a = a1;
+		tol = tol_1;
+	}
+	
+	public double solveRoots(){
+		Horner h=new Horner(a);
+		double f=1, d;
+		while(f>tol){
+			h.evaluate(p);
+			f = h.val();
+			d = h.der();
+			p = p - f/d;
+		}
+		return p;
+	}
+	
 }
